d22814fe7afa4171a224978ef27f15e960cb4561
Christian Ramirez Castillo <chriserc@outlook.com>
2015-04-14 10:32:41 -0500

    Se crea : Capa Datos

---
+++ CapaPresentacion/Compartido/Mensaje.vb
@@ -0,0 +1,13 @@
+﻿
+
+Module Mensaje
+    Public Sub MensajeInformacion(mensaje As String)
+        MessageBox.Show(mensaje, "Información", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+    Public Sub MensajeError(mensaje As String)
+        MessageBox.Show(mensaje, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+    End Sub
+    Public Sub MensajeExclamacion(mensaje As String)
+        MessageBox.Show(mensaje, "Revisar", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+    End Sub
+End Module
